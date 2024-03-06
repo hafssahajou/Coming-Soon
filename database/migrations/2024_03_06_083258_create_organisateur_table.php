54d9ec8b59995d90aa44a364764323fd7f04ac21
hafssahajou <hafssahajou200@gmail.com>
2024-03-06 09:33:56 +0100

    migration organisateur

--- database/migrations/2024_03_06_083258_create_organisateur_table.php
+++ database/migrations/2024_03_06_083258_create_organisateur_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('organisateur', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
             $table->timestamps();
         });
     }
