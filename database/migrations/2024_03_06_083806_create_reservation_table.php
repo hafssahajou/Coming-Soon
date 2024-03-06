6b8d45e1aa9556eaeb7f290fe4819493421bd5da
hafssahajou <hafssahajou200@gmail.com>
2024-03-06 09:41:31 +0100

    migration reservation

--- database/migrations/2024_03_06_083806_create_reservation_table.php
+++ database/migrations/2024_03_06_083806_create_reservation_table.php
@@ -8,19 +8,27 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('reservation', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("event_id")->constrained('event')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("organisateur_id")->constrained('organisateur')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("client_id")->constrained('client')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('status');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('reservation');
     }
