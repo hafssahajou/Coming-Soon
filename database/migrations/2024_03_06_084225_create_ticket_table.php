501e86e8ee80c37700592aa0f1156d6f21b1262b
hafssahajou <hafssahajou200@gmail.com>
2024-03-06 09:44:00 +0100

    finish migration

--- database/migrations/2024_03_06_084225_create_ticket_table.php
+++ database/migrations/2024_03_06_084225_create_ticket_table.php
@@ -8,19 +8,29 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('ticket', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("reservation_id")->constrained('reservation')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('name');
+            $table->text('description');
+            $table->string('place');
+            $table->dateTime('date');
+            $table->string('location');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('ticket');
     }
