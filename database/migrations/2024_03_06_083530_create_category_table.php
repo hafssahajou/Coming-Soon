c57abfd895c422301dfbe6067bee367d6191599e
hafssahajou <hafssahajou200@gmail.com>
2024-03-06 09:36:22 +0100

    migration category

--- database/migrations/2024_03_06_083530_create_category_table.php
+++ database/migrations/2024_03_06_083530_create_category_table.php
@@ -8,20 +8,25 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('category', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('category');
+        Schema::dropIfExists('_category');
     }
 };
