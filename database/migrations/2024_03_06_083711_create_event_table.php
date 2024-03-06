90dae19ba75f64cadaae61c1253cfa65afa85e7b
hafssahajou <hafssahajou200@gmail.com>
2024-03-06 09:37:49 +0100

    migration event

---
+++ database/migrations/2024_03_06_083711_create_event_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("organisateur_id")->constrained('organisateur')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("category_id")->constrained('category')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('date');
+            $table->string('availablity_status');
+            $table->string('location');
+            $table->string('type_of_reservation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+};
