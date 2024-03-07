ec3b325a693b945aeb2eaef567ff18a9011057fe
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:50:03 +0100

    modification

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
     //
+    public function destroy(Request $request) {
+        $reservationId = $request->input('reservation_id');
+        $reservation = Reservation::findOrFail($reservationId);
+        $reservation->delete();
+
+        return redirect('/client-dashboard');
+    }
+   public function store(Request $request){
+    $data = $request->validate([
+        'organisateur_id' => 'required',
+        'event_id' => 'required',
+        'client_id' => 'required',
+        'time' => 'required',
+        'location' => 'required',
+        
+    ]);
+
+
+
+    Reservation:: create($data);
+    return redirect('/organisateur-dashboard');
+}
 }
