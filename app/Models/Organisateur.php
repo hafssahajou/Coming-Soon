918ee6de4a27798b8dff9cbb981ce783e85603eb
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:18:18 +0100

    modification

--- app/Models/Organisateur.php
+++ app/Models/Organisateur.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Organisateur extends Model
+class Organisateur extends User
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'phone_number',
+        'address',
+        'user_id',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    
+    public function event()
+    {
+        return $this->hasMany(Event::class, 'event_id');
+    }
 }
