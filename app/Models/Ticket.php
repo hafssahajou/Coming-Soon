b32a39b0d542114633e5a451062b2face472ca50
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:22:22 +0100

    modification

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,7 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ticket extends Model
+class Event extends Model
 {
-    use HasFactory;
-}
+    
+        use HasFactory;
+        protected $guarded = [];
+
+        public function category()
+        {
+            return $this->belongsTo(category::class, 'category_id');
+        }
+        
+        public function Organisateur()
+        {
+            return $this->belongsTo(organisateur::class, 'organisateur_id');
+        }
+    }
+    
+    
