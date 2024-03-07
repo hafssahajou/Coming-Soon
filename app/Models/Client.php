23caf7eddeb5255d679deda78598b48e5c99e1ec
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:15:28 +0100

    modification

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Client extends Model
+class Client extends User
 {
     use HasFactory;
+   
+     protected $guarded = [];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function reservation()
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
