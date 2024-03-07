6918486454d5375c46c1193fefc2fb8c184b5923
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:02:16 +0100

    change user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,45 +1,30 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as AuthenticatableUser;
 
-class User extends Authenticatable
+class User extends AuthenticatableUser implements Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    // protected $guarded = [];
     protected $fillable = [
         'name',
         'email',
         'password',
+        'picture',
+        
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function Organisateur()
+    {
+        return $this->hasMany(Organisateur::class);
+    }
+    public function Client()
+    {
+        return $this->hasMany(Client::class);
+    }
+    
 }
