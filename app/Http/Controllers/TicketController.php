402a43f5d6b814e37064e002c8d8831b555fcd4a
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:51:16 +0100

    modification

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Models\Ticket;
 
 class TicketController extends Controller
 {
     //
+    public function destroy(Request $request) {
+        $ticketId = $request->input('ticket_id');
+        $ticket = Ticket::findOrFail($ticketId);
+        $ticket->delete();
+
+        return redirect('/organisateur-dashboard');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            'reservation_id' => 'required',
+            'name' => 'required',
+            'description' => 'required',
+            'place' => 'required',
+            'date' => 'required',
+            'location' => 'required',
+        ]);
+
+        Ticket::create($data);
+        return redirect('/organisateur-dashboard');
+    }
 }
