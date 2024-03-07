7c2c31c31ce6a02ed09c86b389c9d22df0b4e32e
hafssahajou <hafssahajou200@gmail.com>
2024-03-07 09:49:00 +0100

    modification

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,5 +6,49 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
-}
+  /**
+         * Remove the specified event from storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\Response
+         */
+        public function destroy(Request $request) 
+        {
+            $eventId = $request->input('event_id');
+            
+            $event = Event::findOrFail($eventId);
+            $event->delete();
+    
+            return redirect('/client-dashboard')->with('success', 'Event deleted successfully');
+        }
+       
+        /**
+         * Store a newly created event in storage.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+            $data = $request->validate([
+                'organisateur_id' => 'required',
+                'category_id' => 'required',
+                'title' => 'required',
+                'description' => 'required',
+                'date' => 'required|date',
+                'availability_status' => 'required',
+                'location' => 'required',
+                'type_of_reservation' => 'required',
+            ]);
+    
+            // Create a new event instance and save it to the database
+            $event = Event::create($data);
+    
+            if ($event) {
+                return redirect('/organisateur-dashboard')->with('success', 'Event created successfully');
+            } else {
+                return redirect()->back()->withInput()->withErrors(['An error occurred while creating the event']);
+            }
+        }
+    }
+    
